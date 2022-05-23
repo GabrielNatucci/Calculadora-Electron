8ea1da9209a31f3584b4e075dd4d42822b102b39
GabrielToledo <natucci@pop-os.localdomain>
2022-05-23 15:09:29 -0300

    Soma funcionando

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,6 @@
     ALGORITMO PARA ENTENDER A SOMA E DIVISÃO
     CONSERTAR DIVISÃO, QUE ADICIONA AO OPERADO.INNERHTML MESMO QUANDO É ZERO
 */
-
-
 // -- Fechar e abrir página -- //
 const {ipcRenderer} = require('electron')
 const ipc = ipcRenderer
@@ -23,6 +21,10 @@ function minimizeWindow () {
 calculado = document.getElementById("calculado"); // é o que mostra depois de algum operador ser clicado
 mostrado = document.getElementById("mostrado"); // é o que mostra o número que está sendo escrito
 calculadora = document.getElementById("calculadora-id"); // variável que guarda todos os botões
+var operar = 0; // para saber quando pode executar as operações
+var primeiroValor;
+var segundoValor;
+var temp;
 
 function escreverNúmero(num){
     if (mostrado.innerHTML == 0){ // se for zero o número que está lá, ele subistituirá esse valor com o digitado
@@ -32,14 +34,39 @@ function escreverNúmero(num){
     }
 }
 
-function calcularNumero(operador){
-    console.log(calculado.innerHTML)
-    if (calculado.innerHTML == 0){ // mesma lógica do de cima, só que com o operador aparecendo também
-        calculado.innerHTML = mostrado.innerHTML + operador;
-    } else {
-        calculado.innerHTML += mostrado.innerHTML + operador;
+function processoParaMostrar(operador){
+    if (mostrado.innerHTML != 0){ // esse if é para ver se tem alguma coisa escrita para adiconar em cima
+        if (calculado.innerHTML == 0){ // mesma lógica do de cima, só que com o operador aparecendo também
+            calculado.innerHTML = mostrado.innerHTML + operador;
+        } else {
+            calculado.innerHTML += mostrado.innerHTML + operador;
+        }
+        operar = 1
     }
-    mostrado.innerHTML = 0;
+}
+
+function calcularNumero(operador){
+    if (operar == 0){
+        primeiroValor = parseInt(mostrado.innerHTML);
+        processoParaMostrar(operador);
+        mostrado.innerHTML = 0;
+    } else {
+        segundoValor = parseInt(mostrado.innerHTML);
+        calculado.innerHTML = parseInt(primeiroValor) + parseInt(segundoValor);
+        primeiroValor = calculado.innerHTML;
+        mostrado.innerHTML = 0;
+    }
+    console.log("Primeiro número: " + primeiroValor);
+    console.log("Segundo número: " + segundoValor);
+}
+
+function calcularIgual(){ // esse é reponsável por calcular quando o operador igual é acionado
+    //let operado = calculado.innerHTML[calculado.innerHTML.length - 1];
+    if (operar == 1){
+        // do thing
+    } else {
+        // else thing
+    }operar = 0;
 }
 
 calculadora.addEventListener("click", function(event){
@@ -52,12 +79,23 @@ calculadora.addEventListener("click", function(event){
             case "clear": // caso o C tenha sido clicado
                 mostrado.innerHTML = 0;
                 calculado.innerHTML = 0;
+                operar = 0;
                 break;
             case "somar": // caso o SOMAR tenha sido clicado
                 calcularNumero("+");
                 break;
             case "dividir":
                 calcularNumero("/");
+                break;
+            case "multiplicar":
+                calcularNumero("*");
+                break;
+            case "subtrair":
+                calcularNumero("-");
+                break;
+            case "igual_a":
+                calcularIgual();
+                operar = 0;
                 break;
         }
     }
